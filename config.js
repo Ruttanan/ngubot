96a5a29f00c113be2a0af78ab598ef0144832c91
Ruttanan <ruttanan.w@gmail.com>
2025-06-14 21:06:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const memberRealNames = {
+    HappyBT: ["Boss", "บอส"],
+    "Dr. Feelgood": ["Pun", "ปั้น"],
+    padkapaow: ["Tun", "ตั้น"],
+    BoonP1: ["Boon", "บุ๋น"],
+    orengipratuu: ["Faye", "ฟาเย่"],
+    imminicosmic: ["Mini", "มินิ"],
+    keffv1: ["Kevin", "เควิน"],
+    keyfungus: ["Ngu", "งู"],
+    soybeant0fu: ["Pookpik", "ปุ๊กปิ๊ก"],
+    ยักcute: ["Geng", "เก่ง"],
+    "ํืUnclejoe": ["Aim", "เอม"],
+    "abobo.gimbo": ["Gimbo", "กิมโบ้"]
+};
+
+module.exports = { memberRealNames };
